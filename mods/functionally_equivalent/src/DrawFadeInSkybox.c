80b571a8a71738661b950d8d33009f1459457de3
celophi <celophi@dakko.me>
2024-01-29 22:34:59 -0800

    Formatting and removing unused variables

--- mods/functionally_equivalent/src/DrawFadeInSkybox.c
+++ mods/functionally_equivalent/src/DrawFadeInSkybox.c
@@ -148,35 +148,21 @@ void DrawFadeInSkybox(void)
     int *piVar6;
     uint uVar7;
     int uVar9;
-    SkyboxModel **wadModelEnd;
     uint uVar8;
-    SkyboxModel **modelsToRender;
     int *cursor;
-    int unaff_s0;
     uint uVar10;
-    int unaff_s1;
     uint *puVar11;
-    int unaff_s2;
-    int unaff_s3;
     int pSVar18;
-    int unaff_s4;
     int iVar12;
-    int unaff_s5;
     int iVar13;
     uint *puVar14;
     uint *puVar15;
-    int unaff_s6;
     int iVar16;
-    int unaff_s7;
     byte *pbVar17;
     uint uVar18;
     int *pDVar19;
-    int unaff_s8;
     P_TAG *pPVar19;
     int *pDVar23;
-    int unaff_retaddr;
-    //undefined auStackX_0 [16];
-    SkyboxModel **wadModelStart;
     
                     /* Store the state of all registers */
                     // Store registers. (This doesn't seem to have an effect on the game working, but it's needed for testing.)
@@ -206,7 +192,6 @@ void DrawFadeInSkybox(void)
     _RegisterStorage.RA = ra;
     _RegisterStorage.GP = gp;
 
-    //_RegisterStorage.GP = (int)&_StaticDataStart;
                     /* Load camera and translation vector */
     gte_ldR11R12(_CameraA.R11R12);
     gte_ldR13R21(_CameraA.R13R21);
@@ -214,22 +199,12 @@ void DrawFadeInSkybox(void)
     gte_ldR31R32(_CameraA.R31R32);
     gte_ldR33(_CameraA.R33);
     gte_ldtr(0,0,0);
+
                     /* Create variables to track the models to render, the start and end */
-    modelsToRender = &_SkyboxModelsToRender;
-    wadModelEnd = _SkyboxWadModels + _SkyboxWadModelCount;
-    wadModelStart = _SkyboxWadModels;
-    /*
-    _RegisterStorage.S0 = unaff_s0;
-    _RegisterStorage.S1 = unaff_s1;
-    _RegisterStorage.S2 = unaff_s2;
-    _RegisterStorage.S3 = unaff_s3;
-    _RegisterStorage.S4 = unaff_s4;
-    _RegisterStorage.S5 = unaff_s5;
-    _RegisterStorage.S6 = unaff_s6;
-    _RegisterStorage.S7 = unaff_s7;
-    _RegisterStorage.SP = (int)register0x00000074;
-    _RegisterStorage.FP = unaff_s8;
-    _RegisterStorage.RA = unaff_retaddr;*/
+    SkyboxModel** modelsToRender = &_SkyboxModelsToRender;
+    SkyboxModel** wadModelEnd = _SkyboxWadModels + _SkyboxWadModelCount;
+    SkyboxModel** wadModelStart = _SkyboxWadModels;
+
                     /* Calculate the clipping and determine if the primitive needs to be drawn or not */
     while (cursor = (int *)*wadModelStart, wadModelStart != wadModelEnd) {
         wadModelStart = wadModelStart + 1;
@@ -238,11 +213,13 @@ void DrawFadeInSkybox(void)
         gte_rtps_b();
         gte_stSZ3(depth);
         gte_stMAC3(depth);
+
         if (0 < depth - (short)cursor[1]) {
             *modelsToRender = (SkyboxModel *)cursor;
             modelsToRender = modelsToRender + 1;
         }
     }
+
     *modelsToRender = (SkyboxModel *)0x0;
     gte_ldR11R12(_CameraB.R11R12);
     gte_ldR13R21(_CameraB.R13R21);
@@ -254,30 +231,40 @@ void DrawFadeInSkybox(void)
     uVar18 = 0;
     depth = _DrawSkyboxU4 + -0x400;
     pDVar23 = (int *)pPVar19;
-    do {
-        do {
+
+    do 
+    {
+        do 
+        {
             pSVar18 = (int)*wadModelStart;
             wadModelStart = wadModelStart + 1;
             pDVar19 = (int *)_PrimitiveList;
-            if (pSVar18 == 0) {
+
+            if (pSVar18 == 0) 
+            {
 DrawFadeInSkybox_A:
                 pPVar1 = _PrimitiveStagingStart;
                 _PrimitiveList = pDVar23;
-                if ((P_TAG *)pDVar23 != pPVar19) {
+
+                if ((P_TAG *)pDVar23 != pPVar19)
+                {
                     pPVar4 = _PrimitiveStagingStart[0x7ff].Tail;
                     *pDVar19 = uVar18 ^ 0x80000000;
                     pPVar1[0x7ff].Tail = (P_TAG *)pDVar19;
-                    if (pPVar4 == (P_TAG *)0x0) {
+
+                    if (pPVar4 == (P_TAG *)0x0) 
+                    {
                         pPVar1[0x7ff].Head = pPVar19;
                     }
-                    else {
-                        //*(short *)pPVar4 = (short)pPVar19;
-                        //pPVar4->field_0x2 = (char)((uint)pPVar19 >> 0x10);
+                    else 
+                    {
                         pPVar4->addr = (u_long)pPVar19;
                     }
                 }
+
                 return;
             }
+
             iVar16 = *(int *)(pSVar18 + 8) >> 0x10;
             iVar13 = (int)(short)*(int *)(pSVar18 + 8);
             iVar12 = (int)*(uint *)(pSVar18 + 0xc) >> 0x10;
@@ -286,13 +273,16 @@ DrawFadeInSkybox_A:
             pbVar17 = &_ScratchpadDrawSkybox;
             uVar10 = 0xffffffff;
             piVar6 = &_DrawFadeInSkybox_U0;
+
             gte_ldBFC((_DrawFadeInSkybox_U1 >> 0x10 & 0xff) << 4);
             gte_ldRFC((_DrawFadeInSkybox_U1 & 0xff) << 4);
             gte_ldGFC((_DrawFadeInSkybox_U1 >> 8 & 0xff) << 4);
             gte_ldIR0(_DrawFadeInSkybox_U2);
             puVar11 = puVar14 + 1;
             gte_ldRGB(*puVar14);
-            do {
+
+            do 
+            {
                 gte_dpcs_b();
                 uVar2 = *puVar11;
                 gte_stRGB2(uVar9);
@@ -301,12 +291,15 @@ DrawFadeInSkybox_A:
                 gte_ldRGB(uVar2);
                 piVar6 = piVar6 + 1;
             } while ((uint *)((int)puVar14 + (uVar8 >> 0xe) + 4) != puVar11);
+
             uVar2 = *(uint *)(pSVar18 + 0x18);
             gte_ldVZ0((uVar2 >> 0x15) + iVar12);
             gte_ldVXY0((iVar13 - (uVar2 >> 10 & 0x7ff)) + (iVar16 - (uVar2 & 0x3ff)) * 0x10000);
             uVar2 = *(uint *)(pSVar18 + 0x1c);
             puVar11 = (uint *)(pSVar18 + 0x20);
-            do {
+
+            do 
+            {
                 gte_rtps_b();
                 uVar3 = uVar2 >> 0x15;
                 uVar5 = uVar2 >> 10;
@@ -316,41 +309,60 @@ DrawFadeInSkybox_A:
                 gte_stSXY2(iVar4);
                 gte_ldVXY0((iVar13 - (uVar5 & 0x7ff)) + (iVar16 - uVar7) * 0x10000);
                 uVar3 = iVar4 * 0x20;
-                if ((int)(iVar4 - 0x10000) < 1) {
+
+                if ((int)(iVar4 - 0x10000) < 1) 
+                {
                     uVar3 = uVar3 + 1;
                 }
-                if (-1 < (int)(iVar4 - 0x1000000)) {
+                if (-1 < (int)(iVar4 - 0x1000000)) 
+                {
                     uVar3 = uVar3 + 2;
                 }
-                if ((int)(iVar4 * 0x10000) < 1) {
+                if ((int)(iVar4 * 0x10000) < 1) 
+                {
                     uVar3 = uVar3 + 4;
                 }
                 puVar11 = puVar11 + 1;
-                if (-1 < (int)(iVar4 * 0x10000 + -0x2000000)) {
+                if (-1 < (int)(iVar4 * 0x10000 + -0x2000000)) 
+                {
                     uVar3 = uVar3 + 8;
                 }
+
                 uVar10 = uVar10 & uVar3;
                 *(uint *)pbVar17 = uVar3;
                 pbVar17 = (byte *)((int)pbVar17 + 4);
+
             } while (puVar11 != puVar14 + 2);
         } while ((uVar10 & 0xf) != 0);
+
         puVar14 = (uint *)((uVar8 >> 0xe) + (int)puVar14);
         puVar11 = puVar14;
-        while( true ) {
+
+        while( true ) 
+        {
             uVar10 = *puVar11;
             puVar15 = puVar11 + 2;
-            if (puVar11 == puVar14 + (uVar8 & 0x1fff) * 2) break;
-            if (depth - (int)pDVar23 < 1) {
+
+            if (puVar11 == puVar14 + (uVar8 & 0x1fff) * 2)
+            {
+                break;
+            }
+
+            if (depth - (int)pDVar23 < 1) 
+            {
                 _DrawSkyboxU3 = 1;
                 pDVar19 = (int *)_PrimitiveList;
                 goto DrawFadeInSkybox_A;
             }
+
             uVar2 = *(uint *)((uVar10 >> 0x14) + 0x1f800000);
             uVar3 = *(uint *)((uVar10 >> 10 & 0x3fc) + 0x1f800000);
             uVar10 = *(uint *)((uVar10 & 0x3fc) + 0x1f800000);
             uVar5 = puVar11[1];
             puVar11 = puVar15;
-            if ((uVar2 & uVar3 & uVar10 & 0x1f) == 0) {
+
+            if ((uVar2 & uVar3 & uVar10 & 0x1f) == 0) 
+            {
                 *(uint *)_PrimitiveList = uVar18 ^ (uint)pDVar23;
                 iVar12 = (int)uVar2 >> 5;
                 iVar13 = (int)uVar3 >> 5;
@@ -361,7 +373,9 @@ DrawFadeInSkybox_A:
                 uVar10 = uVar5 >> 0x14;
                 uVar18 = uVar5 >> 10 & 0x3fc;
                 _PrimitiveList = pDVar23;
-                if ((uVar10 == uVar18) && (uVar10 == (uVar5 & 0x3fc))) {
+
+                if ((uVar10 == uVar18) && (uVar10 == (uVar5 & 0x3fc))) 
+                {
                     uVar18 = 0x84000000;
                     pDVar23[1] = *(int *)((int)&_DrawFadeInSkybox_U0 + uVar10) + -0x10000000;
                     pDVar23[2] = iVar12;
@@ -369,7 +383,8 @@ DrawFadeInSkybox_A:
                     pDVar23[4] = iVar16;
                     pDVar23 = pDVar23 + 5;
                 }
-                else {
+                else 
+                {
                     iVar12 = *(int *)((int)&_DrawFadeInSkybox_U0 + uVar18);
                     iVar13 = *(int *)((int)&_DrawFadeInSkybox_U0 + (uVar5 & 0x3fc));
                     uVar18 = 0x86000000;
